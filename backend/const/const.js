b2f5e41ea189848148f9059fd47b63d65985f16a
Geki Nawaii <ilovebhgaming@gmail.com>
2025-11-18 17:11:23 +0700

    deleted

--- backend/const/const.js
+++
@@ -1,3 +0,0 @@
-export const CHOTOT_BASE_URL = "https://gateway.chotot.com/v1/public/ad-listing";
-export const EBAY_BASE_URL = "https://www.ebay.com/sch/i.html?";
-export const TIKI_BASE_URL = "https://tiki.vn/api/v2/products";
